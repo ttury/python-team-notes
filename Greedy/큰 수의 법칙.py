278f8677cd45a09d0da8968392a8cb250502bf4f
repl.it user <replituser@example.com>
2020-08-22 12:29:40 +0000

    replit test1

--- Greedy/큰 수의 법칙.py
+++ Greedy/큰 수의 법칙.py
@@ -1,1 +1,23 @@
-print("hi")
+import time
+
+n, m, k = map(int, input().split())
+array = list(map(int, input().split()))
+
+array.sort(reverse = True)
+result = 0
+j = 0
+
+start_time = time.time()
+
+for _ in range(m):
+  if(j >= k):
+    result += array[1]
+    j = 0
+    continue
+  result += array[0]
+  j += 1
+
+end_time = time.time()
+
+print(result)
+print(f'소요 시간:{end_time - start_time}')
